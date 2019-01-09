3dc07ad4287877ffecf49c797bf9371dd7e14cb5
DerDominik <derdominik24@googlemail.com>
2019-01-09 14:39:58 +0100

    Change printer mechanic settings
    - Bedsize
    - Endstop settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -885,15 +885,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 215
-#define Y_BED_SIZE 215
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 210
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -3
-#define Y_MIN_POS 0
+#define X_MIN_POS -7
+#define Y_MIN_POS -3
 #define Z_MIN_POS 0
-#define X_MAX_POS 215
-#define Y_MAX_POS 215
+#define X_MAX_POS X_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 205
 
 /**
